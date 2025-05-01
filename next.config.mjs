9212a81f1985e3974b014d10e72ac4afbd49ac33
Jordan <39-12602@sae.edu.za>
2025-05-01 10:47:38 +0200

    convert to pws

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.mjs
+import nextPWA from 'next-pwa';
+import { join } from 'path';
 
-export default nextConfig;
+const withPWA = nextPWA({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+  buildExcludes: [/middleware-manifest\.json$/],
+});
+
+const nextConfig = {
+  reactStrictMode: true,
+  // Add any other Next.js config here
+};
+
+export default withPWA(nextConfig);
